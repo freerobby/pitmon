c76a50404cc84501cd3de0056dfab039df15cb21
Scott Emmons <scotte@users.noreply.github.com>
2014-08-08 13:38:19 -0700

    Minor test update (remove trailing /, coerce temp as degrees)

--- test/test.py
+++ test/test.py
@@ -14,13 +14,13 @@
 from cyberqinterface.cyberqinterface import CyberQInterface
 
 print "== status.xml =="
-result = CyberQInterface("127.0.0.1:8000/static/").getStatus()
+result = CyberQInterface("127.0.0.1:8000/static").getStatus()
 print "Output percent:", result.OUTPUT_PERCENT
-print "Cook temp:", result.COOK_TEMP
-print "Food1 temp:", result.FOOD1_TEMP
+print "Cook temp:", float(result.COOK_TEMP)/10
+print "Food1 temp:", float(result.FOOD1_TEMP)/10
 
 print "\n== all.xml =="
-result = CyberQInterface("127.0.0.1:8000/static/").getAll()
+result = CyberQInterface("127.0.0.1:8000/static").getAll()
 print "Output percent:", result.OUTPUT_PERCENT
-print "Cook temp:", result.COOK.COOK_TEMP
-print "Food1 temp:", result.FOOD1.FOOD1_TEMP
+print "Cook temp:", float(result.COOK.COOK_TEMP)/10
+print "Food1 temp:", float(result.FOOD1.FOOD1_TEMP)/10
