ac17855f3079dfa12a696b28e0cca29f5a999cce
Scott Emmons <scotte@users.noreply.github.com>
2014-08-09 08:50:14 -0700

    initial graph of temperatures working

--- pitmon/urls.py
+++ pitmon/urls.py
@@ -3,6 +3,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     url(r'^$', 'pitmon.views.home'),
+    url(r'^plot$', 'pitmon.views.plot'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
--- pitmon/views.py
+++ pitmon/views.py
@@ -1,6 +1,49 @@
-from __future__ import unicode_literals
+import json
+import matplotlib.pyplot as plt
 from django.http import HttpResponse
+from django.template import loader, Context
 
 
 def home(request):
-    return HttpResponse("OK", content_type='text/html')
+    t = loader.get_template('home.html')
+    c = Context({
+    })
+    return HttpResponse(t.render(c))
+
+
+def plot(request):
+    response = HttpResponse(content_type='image/png')
+
+    statefile = open('/tmp/pitmon.json')
+    readings = json.load(statefile)
+    x = []
+    y1 = []
+    y2 = []
+    y2 = []
+    y3 = []
+    y4 = []
+    l = []
+    t = []
+
+    idx = 0
+    tick = 0
+    intvl = len(readings)/10
+    for reading in readings:
+        x.append(idx)
+        y1.append(reading['COOK_TEMP'])
+        y2.append(reading['FOOD1_TEMP'])
+        y3.append(reading['FOOD2_TEMP'])
+        y4.append(reading['FOOD3_TEMP'])
+        if idx % intvl == 0:
+            t.append(idx)
+            l.append(reading['TIME'][:5])
+            tick += 1
+        idx += 1
+
+    plt.plot(x, y1, "rs", x, y2, "b^", x, y3, "gp", x, y4, "mD")
+    plt.ylabel('Temperature F')
+    plt.xticks(t, l)
+    plt.savefig(response, dpi=100)
+    plt.close()
+
+    return response
