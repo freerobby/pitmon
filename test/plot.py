bd2f7afb5479868986d3681328fa589079e7022d
Scott Emmons <scotte@users.noreply.github.com>
2014-08-09 08:26:39 -0700

    Plotting prototype

---
+++ test/plot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+
+import json
+import matplotlib.pyplot as plt
+
+statefile = open('/tmp/pitmon.json')
+readings = json.load(statefile)
+x = []
+y1 = []
+y2 = []
+y2 = []
+y3 = []
+y4 = []
+l = []
+t = []
+
+idx = 0
+tick = 0
+for reading in readings:
+    x.append(idx)
+    y1.append(reading['COOK_TEMP'])
+    y2.append(reading['FOOD1_TEMP'])
+    y3.append(reading['FOOD2_TEMP'])
+    y4.append(reading['FOOD3_TEMP'])
+    if idx % 5 == 0:
+        t.append(idx)
+        l.append(reading['TIME'][:5])
+        tick += 1
+    idx += 1
+
+plt.plot(x, y1, "rs", x, y2, "b^", x, y3, "gp", x, y4, "mD")
+plt.ylabel('Temperature F')
+plt.xticks(t, l)
+plt.show()
