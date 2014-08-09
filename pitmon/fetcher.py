6efa395bbd601fbc76fd2fb82df5f36a976b2535
Scott Emmons <scotte@users.noreply.github.com>
2014-08-08 22:11:37 -0700

    Cleanup in fetcher

--- pitmon/fetcher.py
+++ pitmon/fetcher.py
@@ -5,18 +5,15 @@ import os.path
 from cyberqinterface.cyberqinterface import CyberQInterface
 
 
+# TODO: param url and json file
 class Fetcher(threading.Thread):
 
     def run(self):
 
-        probes = ['COOK', 'FOOD1', 'FOOD2', 'FOOD3']
-        temps = ['TEMP', 'SET']
-        statuses = ['COOK_STATUS', 'FOOD1_STATUS',
-                    'FOOD2_STATUS', 'FOOD3_STATUS']
-        generals = ['OUTPUT_PERCENT']
-
         cyberq = CyberQInterface("127.0.0.1:8000/static")
 
+        # If file exists, try to load it. If it doesn't or
+        # load fails, start a new file
         if (os.path.isfile('/tmp/pitmon.json')):
             statefile = open('/tmp/pitmon.json')
             try:
@@ -26,12 +23,16 @@ class Fetcher(threading.Thread):
             statefile.close()
         else:
             readings = []
+
         while True:
             result = cyberq.getAll()
             print "Output percent:", result.OUTPUT_PERCENT
             print "Output percent:", result['OUTPUT_PERCENT']
             print "Cook temp:", float(result.COOK.COOK_TEMP)/10
             print "Food1 temp:", float(result.FOOD1.FOOD1_TEMP)/10
+
+            probes = ['COOK', 'FOOD1', 'FOOD2', 'FOOD3']
+            temps = ['TEMP', 'SET']
 
             reading = dict()
             for probe in probes:
@@ -40,8 +41,7 @@ class Fetcher(threading.Thread):
                     reading[key] = float(result[probe][key])/10
                 key = '%s_STATUS' % probe
                 reading[key] = cyberq.statusLookup(result[probe][key])
-            for general in generals:
-                reading[general] = int(result[general])
+            reading['OUTPUT_PERCENT'] = int(result['OUTPUT_PERCENT'])
             readings.append(reading)
 
             statefile = open('/tmp/pitmon.json', 'w')
