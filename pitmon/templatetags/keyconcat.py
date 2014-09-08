3f5bee359d4f427040b49a13f1fd369e6b22bf34
Scott Emmons <scotte@users.noreply.github.com>
2014-09-07 17:43:52 -0700

    Improve tags, no temp display if NO PROBE

--- pitmon/templatetags/keyconcat.py
+++ pitmon/templatetags/keyconcat.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.simple_tag
 def get_key(obj, val1, val2):
     return obj['%s%s' % (val1, val2)]
+
+
+@register.assignment_tag
+def assign_key(obj, val1, val2):
+    return obj['%s%s' % (val1, val2)]
